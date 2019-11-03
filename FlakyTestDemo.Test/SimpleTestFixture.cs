b4e91925111c0e173428f34c2a1bcf2d35ea118f
Dushyant Priyadarshee <dushyant.priyadarshee@sharp-ls.eu>
2019-11-03 11:23:31 +0000

    Add unit test solution and azure pipeline.

---
+++ FlakyTestDemo.Test/SimpleTestFixture.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using NUnit.Framework.Constraints;
+
+namespace FlakyTestDemo.Test
+{
+    [TestFixture]
+    public class SimpleTestFixture
+    {
+        [Test]
+        public void DeliberateFlakyTest()
+        {
+            var randomNumber = new Random().Next();
+            var result = randomNumber % 5;
+
+            Assert.That(result, Is.Not.EqualTo(0));
+        }
+
+        [Test]
+        public void NotFlakyTest()
+        {
+            Assert.That(0, Is.EqualTo(0));
+        }
+    }
+}
